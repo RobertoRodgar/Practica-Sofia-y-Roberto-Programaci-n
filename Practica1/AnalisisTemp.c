8c962844de3e7fabd1201610621b2eef8728b6fc
RobertoRodgar <roberto.rodriguez.dosa@gmail.com>
2024-11-06 11:31:27 +0100

    Practicas

--- Practica1/AnalisisTemp.c
+++ Practica1/AnalisisTemp.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-	int main(){
+
+int main(){
 
 	int guardar_dia;
 	int contador = 0;
@@ -10,20 +11,20 @@
 	float promedio;
 	float temp_dia[7];
 
-	char dias[7][10] = {"Lunes","Martes","Miercoles","Jueves","Viernes","Sábado","Domingo"}; //el primer valor es la cantidad de variables  y el segundo la longitud máxima.
+	char dias[7][10] = {"lunes","martes","miercoles","jueves","viernes","sábado","domingo"}; //el primer valor es la cantidad de variables  y el segundo la longitud máxima.
 	for (int i = 0; i < 7; i++){ //Función que pide las temperaturas de cada día.
 
 		printf("Introduce el promedio de temperatura del %s: ", dias[i]);
 		scanf("%f", &temp_dia[i]);
 	}
-	
+
 	for (int i = 0; i < 7; i++){ //Función que suma cada día y calcula su promedio.
 		promedio += temp_dia[i];
 	}
 	promedio = promedio / 7;
 
 	printf("Temperatura promedio de la semana: %0.1fºC\n", promedio);
-	
+
 	for (int i = 0; i < 7; i++){ //Función que sobrescribe la funcion maxima y la cambia por la mas alta
 		if (temp_dia[i] > maxima){
 			maxima = temp_dia[i];
@@ -32,7 +33,7 @@
 		}
 	}
 	printf("Temperatura máxima: %dºC del día %s.\n",maxima, dias[guardar_dia]);
-		
+
 	for (int i = 0; i < 7; i++){ //Lo mismo que la máxima pero con la mínima.
 		if (temp_dia[i] < minima){
 			minima = temp_dia[i];
@@ -51,12 +52,14 @@
 	printf("Número de días con temperaturas superiores al promedio: %d\n", contador);
 
 	printf("Dias con temperatura por debajo del promedio: \n");
-	for (int i = 0; i < 7; i ++){ //Función que imprime los días por debajo del promedio.
+	for (int i = 0; i < 7; i++){ //Función que imprime los días por debajo del promedio.
 		if (temp_dia[i] < promedio){
-			printf("%s: %0.1fºC\n", dias[i], temp_dia[i]);
+			char letra_mayuscula = dias[i][0] - 'a' + 'A';
+			dias[i][0] = letra_mayuscula;
+			printf("%s: %0.1fºC \n", dias[i], temp_dia[i]);
 		} else {
 		}
 	}
 
 	return 0;
-	}
+}
--- Practica2/AnalisisEstudiantes.c
+++ Practica2/AnalisisEstudiantes.c
@@ -1,24 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define aprobado 5.0 
+
 int main(){
 	int num_estudiantes[100];
 	int reemplazo;
 	double nota_final;
-	int promedio;
+	double promedio;
+	int cuenta_aprobado = 0;
+	int cuenta_suspenso = 0;
 	printf("Introduce el número de estudiantes: ");
 	scanf("%d", &reemplazo);
 
-	num_estudiantes[100] = reemplazo + 1;
+	num_estudiantes[100] = reemplazo - 1;
 
 	for (int i = 0; i < reemplazo; i++){
 	printf("Introduce la nota final de cada alumno: ");
-	scanf("%d", &nota_final);
+	scanf("%lf", &nota_final);
 	num_estudiantes[i] = nota_final;
 	}
 	
 	for (int i = 0; i < reemplazo; i++){
-		promedio += 
-	return 0;
-}
+		promedio += num_estudiantes[i];
+	}
+	promedio = promedio / reemplazo;
+	printf("Promedio de notas: %0.2f\n", promedio);
 
+	for (int i = 0;i < reemplazo; i++){
+		if (num_estudiantes[i] >= aprobado){
+			cuenta_aprobado += 1;
+		}
+	}
+	printf("Número de estudiantes aprobados: %d\n", cuenta_aprobado);
+
+}
