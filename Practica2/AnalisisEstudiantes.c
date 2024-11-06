d3d151d8e2ab33f6c09429833a7935d8ce85f38f
RobertoRodgar <roberto.rodriguez.dosa@gmail.com>
2024-11-06 14:30:49 +0100

    Practicas

--- Practica2/AnalisisEstudiantes.c
+++ Practica2/AnalisisEstudiantes.c
@@ -4,12 +4,15 @@
 #define aprobado 5.0 
 
 int main(){
-	int num_estudiantes[100];
+	double num_estudiantes[100];
 	int reemplazo;
 	double nota_final;
 	double promedio;
 	int cuenta_aprobado = 0;
 	int cuenta_suspenso = 0;
+	double maxima = 0;
+	double minima = 100;
+	int estudiante;
 	printf("Introduce el número de estudiantes: ");
 	scanf("%d", &reemplazo);
 
@@ -34,4 +37,26 @@ int main(){
 	}
 	printf("Número de estudiantes aprobados: %d\n", cuenta_aprobado);
 
+	for (int i = 0;i < reemplazo; i++){
+		if (num_estudiantes[i] < aprobado){
+			cuenta_suspenso += 1;
+		}
+	}
+	printf("Número de estudiantes supendidos: %d\n", cuenta_suspenso);
+
+	for (int i = 0; i < reemplazo; i++){
+		if (num_estudiantes[i] > maxima){
+			maxima = num_estudiantes[i];
+			estudiante = i + 1;
+		}
+	}
+	printf("Nota máxima: %0.1f (Estudiante en la posición %d) \n", maxima, estudiante);
+	
+	for (int i = 0; i < reemplazo; i++){
+		if (num_estudiantes[i] < minima){
+			minima = num_estudiantes[i];
+			estudiante = i + 1;
+		}
+	}
+	printf("Nota mínima: %0.1f (Estudiante en la posición %d) \n", minima, estudiante);
 }
