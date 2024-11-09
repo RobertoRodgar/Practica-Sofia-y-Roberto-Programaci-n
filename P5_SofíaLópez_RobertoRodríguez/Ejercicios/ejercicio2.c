ac056c0a3f20fc4a03c880598de0c5e7ca906308
Sofia1316 <sofiiaa1316@gmail.com>
2024-11-09 17:38:07 +0100

    ejercicio2

--- P5_SofíaLópez_RobertoRodríguez/Ejercicios/ejercicio2.c
+++ P5_SofíaLópez_RobertoRodríguez/Ejercicios/ejercicio2.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(){
 	
 	//Lista de palabras aleatorias
-	char palabra[5][10] = {"manzana","limón","uva","melón","lima"};
+	char palabra[5][8] = {"manzana","limón","uva","melón","lima"};
 
-	//
+	//Recorrido por la palabra 
 	for(int i = 0; i < 5; i++){	
-		if(palabra == 'a'){
-			continue;
-		} else 
-		printf("%s", palabra[i]);
+		for (int a = 0; a < strlen(palabra[i]); a++){	
+			if(palabra[i][a] != 'a'){
+				continue;
+			} 
+		}
+		printf("%s\n", palabra[i]);
 	}
 
 return EXIT_SUCCESS;
